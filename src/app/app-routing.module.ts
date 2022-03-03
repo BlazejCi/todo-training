9ecc881b3d0329870930c49a3670ca7299d42de4
BlazejCi <polandmusic100@gmail.com>
2022-03-03 18:43:33 +0100

    page about-us

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'onas', 
+        loadChildren: () => AboutUsPageModule
+      }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
